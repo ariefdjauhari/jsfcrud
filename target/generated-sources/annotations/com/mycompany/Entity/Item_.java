05944e443e03edd71241416175d4f86869b6504f
ariefdjauhari <arief.djauhari@gmail.com>
2017-11-20 11:26:56 +0700

    fix package test

--- target/generated-sources/annotations/com/mycompany/Entity/Item_.java
+++ target/generated-sources/annotations/com/mycompany/Entity/Item_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-17T19:41:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-20T11:25:49")
 @StaticMetamodel(Item.class)
 public class Item_ { 
 
--- target/generated-sources/annotations/com/mycompany/Entity/OrderedItem_.java
+++ target/generated-sources/annotations/com/mycompany/Entity/OrderedItem_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-17T19:41:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-20T11:25:49")
 @StaticMetamodel(OrderedItem.class)
 public class OrderedItem_ { 
 
